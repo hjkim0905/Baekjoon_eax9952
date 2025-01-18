fba11da54f1f3d2fafa08923f4057636c94d9b63
hjkim0905 <eax9952@gmail.com>
2025-01-18 20:30:53 +0900

    [Feat] : Baekjoon A082 Done

---
+++ Week3/A082_김현중_20250117.java
@@ -0,0 +1,38 @@
+//Baekjoon 10814
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        String[][] arr = new String[N][2];
+
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            arr[i][0] = st.nextToken();
+            arr[i][1] = st.nextToken();
+        }
+
+        Arrays.sort(arr, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] s1, String[] s2) {
+                return Integer.compare(Integer.parseInt(s1[0]), Integer.parseInt(s2[0]));
+            }
+        });
+
+        for(int i = 0; i < N; i++) {
+            sb.append(arr[i][0]).append(' ').append(arr[i][1]).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
