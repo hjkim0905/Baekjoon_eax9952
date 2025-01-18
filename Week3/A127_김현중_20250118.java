49c59f0c6fdb1223174a5f704db0c14f653f66c7
hjkim0905 <eax9952@gmail.com>
2025-01-18 21:58:11 +0900

    [Feat] : Baekjoon A127 Done

---
+++ Week3/A127_김현중_20250118.java
@@ -0,0 +1,31 @@
+//Baekjoon 1934
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            sb.append(A * B / gcd(A, B)).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+}
