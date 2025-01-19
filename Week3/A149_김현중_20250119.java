c2024639809cdc0122199276638364828762db9d
hjkim0905 <eax9952@gmail.com>
2025-01-19 15:29:56 +0900

    [Feat] : Baekjoon A149 Done

---
+++ Week3/A149_김현중_20250119.java
@@ -0,0 +1,36 @@
+//Baekjoon 7568
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+        int[] rank = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            int cnt = 1;
+            for (int j = 0; j < N; j++) {
+                if (i == j) continue;
+                if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
+                    cnt++;
+                }
+            }
+            rank[i] = cnt;
+        }
+
+        for (int i = 0; i < N; i++) {
+            System.out.print(rank[i] + " ");
+        }
+    }
+}
