916b4b70edb7b1e8f61ac03a7d7237866c88367a
hjkim0905 <eax9952@gmail.com>
2025-01-19 15:23:13 +0900

    [Feat] : Baekjoon A148 Done

---
+++ Week3/A148_김현중_20250119.java
@@ -0,0 +1,14 @@
+//Baekjoon 1152
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine().trim());
+        System.out.println(st.countTokens());
+    }
+}
