b8e83fe9a765e7cff985ef10be251eb2f87c13a4
hjkim0905 <eax9952@gmail.com>
2025-01-20 17:32:31 +0900

    [Feat] : Baekjoon A153 Done

---
+++ Week3/A153_김현중_20250120.java
@@ -0,0 +1,27 @@
+//Baekjoon 11399
+
+import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] time = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            time[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(time);
+        int sum = 0;
+        int result = 0;
+        for (int i = 0; i < N; i++) {
+            sum += time[i];
+            result += sum;
+        }
+        System.out.println(result);
+    }
+}
