0bb92fb8ca598655840af6cff008cea5a2dc10a9
hjkim0905 <eax9952@gmail.com>
2025-01-27 01:23:59 +0900

    [Fix] : A210 Runtime error fixed

--- Week4-1/A210_김현중_20250126.java
+++ Week4-1/A210_김현중_20250126.java
@@ -9,25 +9,38 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
         for (int i = 0; i < T; i++) {
             int n = Integer.parseInt(br.readLine());
             int[][] sticker = new int[2][n];
             int[][] dp = new int[2][n];
+
             for (int j = 0; j < 2; j++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 for (int k = 0; k < n; k++) {
                     sticker[j][k] = Integer.parseInt(st.nextToken());
                 }
             }
+
+            if (n == 1) {
+                sb.append(Math.max(sticker[0][0], sticker[1][0])).append("\n");
+                continue;
+            }
+
             dp[0][0] = sticker[0][0];
             dp[1][0] = sticker[1][0];
             dp[0][1] = dp[1][0] + sticker[0][1];
             dp[1][1] = dp[0][0] + sticker[1][1];
+
             for (int j = 2; j < n; j++) {
                 dp[0][j] = Math.max(dp[1][j - 1], dp[1][j - 2]) + sticker[0][j];
                 dp[1][j] = Math.max(dp[0][j - 1], dp[0][j - 2]) + sticker[1][j];
             }
-            System.out.println(Math.max(dp[0][n - 1], dp[1][n - 1]));
+
+            sb.append(Math.max(dp[0][n - 1], dp[1][n - 1])).append("\n");
         }
+
+        System.out.print(sb.toString());
     }
 }
