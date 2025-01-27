e89ff91ed34404e8cb33d0b56938f374e71fd37d
hjkim0905 <eax9952@gmail.com>
2025-01-27 22:22:33 +0900

    [Feat] : A220 Done

---
+++ Week4-2/A220_김현중_20250127.java
@@ -0,0 +1,24 @@
+//Baekjoon 2754
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        String[] grade = {"A+", "A0", "A-", "B+", "B0", "B-", "C+", "C0", "C-", "D+", "D0", "D-", "F"};
+        double[] score = {4.3, 4.0, 3.7, 3.3, 3.0, 2.7, 2.3, 2.0, 1.7, 1.3, 1.0, 0.7, 0.0};
+        int index = 0;
+
+        for(int i = 0; i < grade.length; i++) {
+            if(input[0].equals(grade[i])) {
+                index = i;
+                break;
+            }
+        }
+
+        System.out.println(score[index]);
+    }
+}
