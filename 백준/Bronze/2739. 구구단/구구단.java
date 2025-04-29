56b0492d3a86d1eb482ace249c8a75d0db65f268
HJ <143365392+hjkim0905@users.noreply.github.com>
2025-04-29 19:07:43 +0900

    [Bronze V] Title: 구구단, Time: 108 ms, Memory: 14416 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 1; i <= 9; i++) {
+			System.out.printf("%d * %d = %d\n", N, i, N * i);
+		}
+	}
+}
