f228f3dc2261d23692a10912c737ec17ad6d81ca
HJ <143365392+hjkim0905@users.noreply.github.com>
2025-05-16 18:35:13 +0900

    [Bronze III] Title: 직각삼각형, Time: 100 ms, Memory: 14036 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.java
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -1,39 +1,31 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
-    ArrayList<String> result = new ArrayList<>();
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    
-    while (true) {
-      String line = br.readLine();
-      if (line == null || line.isEmpty()) {
-        break;
-      }
-      
-      StringTokenizer st = new StringTokenizer(line);
-      int[] arr = new int[3];
-      
-      for (int i = 0; i < 3; i++) {
-        arr[i] = Integer.parseInt(st.nextToken());
-      }
-      
-      Arrays.sort(arr);
-      
-      if (arr[0] == 0 && arr[1] == 0 && arr[2] == 0) {
-        break;
-      } else {
-        if (Math.pow(arr[0], 2) + Math.pow(arr[1], 2) == Math.pow(arr[2], 2)) {
-          result.add("right");
-        } else {
-          result.add("wrong");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int[] arr = new int[3];
+            int n1 = Integer.parseInt(st.nextToken());
+            int n2 = Integer.parseInt(st.nextToken());
+            int n3 = Integer.parseInt(st.nextToken());
+            if (n1 == 0 && n2 == 0 && n3 == 0)
+                break;
+            else {
+                arr[0] = n1;
+                arr[1] = n2;
+                arr[2] = n3;
+                Arrays.sort(arr);
+                if (Math.pow(arr[0], 2) + Math.pow(arr[1], 2) == Math.pow(arr[2], 2)) {
+                    sb.append("right").append("\n");
+                } else {
+                    sb.append("wrong").append("\n");
+                }
+            }
         }
-      }
+        sb.deleteCharAt(sb.length() - 1);
+        System.out.println(sb);
     }
-    
-    for (String s : result) {
-      System.out.println(s);
-    }
-  }
 }
